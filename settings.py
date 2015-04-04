e2cf036e5cf6b2b247d3249eac951bcb88f519b9
Torrie Fischer <torrie@ripple.com>
2015-04-04 02:56:54 -0700

    Dont show page title if were on homepage

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 """Spacewiki settings"""
 DATABASE = 'spacewiki.sqlite3'
 SITE_NAME = 'SpaceWiki'
-INDEX_PAGE = 'Index'
+INDEX_PAGE = 'index'
 
 try:
     from local_settings import * #pylint: disable=unused-wildcard-import,wildcard-import
