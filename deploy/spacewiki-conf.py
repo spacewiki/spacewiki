fee2d531ed919c55b2b03e9f3bbcf0eefe087bc1
Torrie Fischer <tdfischer@hackerbots.net>
2015-04-10 19:16:54 -0700

    ratchet: Setup upload directories

--- deploy/spacewiki-conf.py
+++ deploy/spacewiki-conf.py
@@ -1,5 +1,6 @@
 DATABASE = '/srv/spacewiki/data/db.sqlite3'
 SITE_NAME = 'Noisebridge'
 INDEX_PAGE = 'main_page'
+UPLOAD_PATH = '/srv/spacewiki/data/uploads'
 
 ADMIN_EMAILS = ['tdfischer@hackerbots.net']
