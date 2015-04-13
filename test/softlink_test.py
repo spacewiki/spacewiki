257d4f010ba1328f89675f0cd2caf760fb352154
Torrie Fischer <tdfischer@hackerbots.net>
2015-04-12 21:44:43 -0700

    Use hypothesis to test softlink parsing

--- test/softlink_test.py
+++ test/softlink_test.py
@@ -3,6 +3,18 @@ import unittest
 import model
 from app import app
 import logging
+import hypothesis
+import string
+from hypothesis.extra.fakefactory import FakeFactory
+
+def makePath(s):
+    return s.split('://', 1)[1].split('/', 1)[1]
+
+Path = hypothesis.strategy(FakeFactory('url')).map(makePath)
+
+FullUrl = hypothesis.strategy(FakeFactory('url'))
+
+Domain = hypothesis.strategy(FakeFactory('domain_name'))
 
 class SoftlinkTestCase(unittest.TestCase):
     def setUp(self):
@@ -12,41 +24,29 @@ class SoftlinkTestCase(unittest.TestCase):
         model.Page.create(title='index', slug='index')
         self.app = app.test_client()
 
-    def test_parser_no_refer(self):
-        req = werkzeug.test.EnvironBuilder('/')
+    @hypothesis.given(Path)
+    def test_parser_no_refer(self, s):
+        req = werkzeug.test.EnvironBuilder(path=s)
         resp = model.Page.parsePreviousSlugFromRequest(req, 'index')
         self.assertEqual(req.lastSlug, None)
 
-    def test_parser_extern_refer(self):
-        req = werkzeug.test.EnvironBuilder('/', headers={'Referer': \
-            'http://example.com/'})
+    @hypothesis.given(Path, FullUrl)
+    def test_parser_extern_refer(self, page, refer):
+        req = werkzeug.test.EnvironBuilder(
+            path=page,
+            headers={
+                'Referer': refer
+            }
+        )
         resp = model.Page.parsePreviousSlugFromRequest(req, 'index')
         self.assertEqual(req.lastSlug, None)
 
-    def test_parser_index_refer(self):
-        req = werkzeug.test.EnvironBuilder('http://example.com/index', headers={'Referer': \
-            'http://example.com/', 'Host': 'example.com'},
-            base_url='http://example.com/').get_request()
-        resp = model.Page.parsePreviousSlugFromRequest(req, 'index')
-        self.assertEqual(req.lastSlug, 'index')
-
-    def test_parser_page_refer(self):
-        req = werkzeug.test.EnvironBuilder('http://example.com/index', headers={'Referer': \
-            'http://example.com/referring-page', 'Host': 'example.com'},
-            base_url='http://example.com/').get_request()
-        resp = model.Page.parsePreviousSlugFromRequest(req, 'index')
-        self.assertEqual(req.lastSlug, 'referring-page')
-
-    def test_parser_subdir(self):
-        req = werkzeug.test.EnvironBuilder('http://example.com/wiki/index', headers={'Referer': \
-            'http://example.com/wiki/referring-page', 'Host': 'example.com'},
-            base_url='http://example.com/wiki/').get_request()
-        resp = model.Page.parsePreviousSlugFromRequest(req, 'index')
-        self.assertEqual(req.lastSlug, 'referring-page')
-
-    def test_parser_subdir(self):
-        req = werkzeug.test.EnvironBuilder('http://example.com/wiki/index', headers={'Referer': \
-            'http://example.com/wiki/referring-page', 'Host': 'example.com'},
-            base_url='http://example.com/wiki/').get_request()
-        resp = model.Page.parsePreviousSlugFromRequest(req, 'index')
-        self.assertEqual(req.lastSlug, 'referring-page')
+    @hypothesis.given(Path, Path, Domain, Path)
+    def test_parser_page_refer(self, page, referPage, host, prefix):
+        req = werkzeug.test.EnvironBuilder(
+            path=prefix+page,
+            headers={'Referer': 'http://'+host+prefix+referPage, 'Host': host},
+            base_url='http://'+host+prefix
+        ).get_request()
+        resp = model.Page.parsePreviousSlugFromRequest(req, referPage)
+        self.assertEqual(req.lastSlug, referPage)
