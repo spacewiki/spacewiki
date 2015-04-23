cdbdba24a71473cf0ee92f83b0f58b5f48a6866b
Torrie Fischer <tdfischer@hackerbots.net>
2015-04-23 16:52:55 -0700

    modulize editor, clean up ace/textarea selector

--- theme/js/editor.js
+++ theme/js/editor.js
@@ -1,32 +1,42 @@
-function useACE(yes) {
-  console.log("ACE: %s", yes);
-  if (yes) {
-    $('#body').hide();
-    $('#editor').show();
-    $('#editor_toggle_value').value=true;
-  } else {
-    $('#body').show();
-    $('#editor').hide();
-    $('#editor_toggle_value').value=false;
-  }
-}
+(function() {
+  var editor;
 
-$(document).ready(function() {
-  console.log($('#editor_toggle_value'));
-  $('#editor_toggle_value').change(function() {
-    useACE($('#editor_toggle_value')[0].checked);
-  });
-  if (ace) {
-    $('#editor_toggle').show();
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/markdown");
-    editor.getSession().on('change', function(e) {
-      $('#body').val(editor.getValue());
-    });
-    $('#body').changed(function() {
-      editor.setValue($('#body').val());
-    });
-    useACE(true);
+  function useACE(yes) {
+    console.log("ACE: %s", yes);
+    if (yes) {
+      $('#body').hide();
+      $('#editor').show();
+      $('#editor_toggle_value')[0].checked = true;
+    } else {
+      $('#body').show();
+      $('#editor').hide();
+      $('#editor_toggle_value')[0].checked = false;
+    }
   }
-});
+
+  function syncBodyToEditor() {
+    editor.setValue($('#body').val());
+
+  }
+
+  function syncEditorToBody() {
+    $('#body').val(editor.getValue());
+  }
+
+  $(document).ready(function() {
+    console.log($('#editor_toggle_value'));
+    if (ace) {
+      $('#editor_toggle').show();
+      editor = ace.edit("editor");
+      editor.setTheme("ace/theme/monokai");
+      editor.getSession().setMode("ace/mode/markdown");
+      editor.getSession().on('change', syncEditorToBody);
+      $('#body').change(syncBodyToEditor);
+      useACE(true);
+    }
+    syncBodyToEditor();
+    $('#editor_toggle_value').change(function() {
+      useACE($('#editor_toggle_value')[0].checked);
+    });
+  });
+})();
