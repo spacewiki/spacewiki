12d9e815850ca5efc7c1aa44670152feed5a12ef
Torrie Fischer <tdfischer@hackerbots.net>
2015-05-01 21:18:04 -0700

    Fix up editor.js errors

--- theme/js/editor.js
+++ theme/js/editor.js
@@ -26,24 +26,25 @@
   }
 
   $(document).ready(function() {
-    console.log($('#editor_toggle_value'));
-    if (ace) {
-      $('#editor_toggle').show();
-      editor = ace.edit("editor");
-      editor.setTheme("ace/theme/monokai");
-      editor.getSession().setMode("ace/mode/markdown");
-      editor.getSession().on('change', syncEditorToBody);
-      $('#body').change(syncBodyToEditor);
-      $.cookie.json = true;
-      if ($.cookie('use-ace')) {
-        useACE(true);
-      } else {
-        useACE(false);
+    if ($('#editor').length > 0) {
+      if (ace) {
+        $('#editor_toggle').show();
+        editor = ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+        editor.getSession().setMode("ace/mode/markdown");
+        editor.getSession().on('change', syncEditorToBody);
+        $('#body').change(syncBodyToEditor);
+        $.cookie.json = true;
+        if ($.cookie('use-ace')) {
+          useACE(true);
+        } else {
+          useACE(false);
+        }
       }
+      syncBodyToEditor();
+      $('#editor_toggle_value').change(function() {
+        useACE($('#editor_toggle_value')[0].checked);
+      });
     }
-    syncBodyToEditor();
-    $('#editor_toggle_value').change(function() {
-      useACE($('#editor_toggle_value')[0].checked);
-    });
   });
 })();
