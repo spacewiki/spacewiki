a01a5a5ba8f2728ef8cb5b5b655ee48dbff51c1b
Torrie Fischer <tdfischer@hackerbots.net>
2016-11-06 20:39:20 -0800

    model: load database URI from environment if present, to support heroku by magic

--- spacewiki/settings.py
+++ spacewiki/settings.py
@@ -1,5 +1,7 @@
 """Spacewiki settings"""
-DATABASE = 'sqlite:///spacewiki.sqlite3'
+import os
+
+DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///spacewiki.sqlite3')
 SITE_NAME = 'SpaceWiki'
 INDEX_PAGE = 'index'
 UPLOAD_PATH = 'uploads'
