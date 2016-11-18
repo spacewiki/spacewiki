377b4bca924aa17a58c48a50e256d9006c1fe525
Torrie Fischer <tdfischer@hackerbots.net>
2016-11-18 08:46:33 -0800

    test: template: use peewee test db utils

--- test/template_test.py
+++ test/template_test.py
@@ -2,13 +2,12 @@ from spacewiki import model, wikiformat
 from spacewiki.wikiformat import directives
 import unittest
 from spacewiki.app import APP as app
+from playhouse.test_utils import test_database
+from peewee import SqliteDatabase
+
+test_db = SqliteDatabase(':memory:')
 
 class ParserTestCase(unittest.TestCase):
-    def setUp(self):
-        app.config['DATABASE'] = 'sqlite:///:memory:'
-        with app.app_context():
-            model.syncdb()
-
     def test_directives(self):
         self.assertEqual(directives.render("", ''), "")
         self.assertEqual(directives.render("{{foo}}", ''), "{{[[foo]]}}")
@@ -17,11 +16,8 @@ class ParserTestCase(unittest.TestCase):
         self.assertEqual(wikiformat.render_wikitext("", ''), "")
 
     def test_recursive_templates(self):
-        try:
+        with test_database(test_db, [model.Page, model.Revision]):
             page = model.Page.create(title='recursive', slug='recursive')
-        except model.Page.DoesNotExist:
-            page = model.Page.get(slug='recursive')
-        page.newRevision('{{recursive}}', '', '')
-        self.assertEqual(directives.render("{{recursive}}", ''),
-                         "{{Max include depth of 11 reached before " + \
-                             "[[recursive]]}}")
+            page.newRevision('{{recursive}}', '', '')
+            canary = "{{Max include depth of"
+            self.assertTrue(canary in directives.render("{{recursive}}", ''))
