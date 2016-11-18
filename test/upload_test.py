5e456a89eb55d30d58b8244d6cbb4ff1eba9b8d0
Torrie Fischer <tdfischer@hackerbots.net>
2016-11-18 09:17:39 -0800

    test: upload: use peewee test db

--- test/upload_test.py
+++ test/upload_test.py
@@ -5,60 +5,66 @@ import tempfile
 import hashlib
 from StringIO import StringIO
 import os
+from playhouse.test_utils import test_database
+from peewee import SqliteDatabase
+
+test_db = SqliteDatabase(':memory:')
 
 class UploadTestCase(unittest.TestCase):
     def setUp(self):
-        app.config['DATABASE'] = 'sqlite:///'+tempfile.mkdtemp()+'/test.sqlite3'
-        with app.app_context():
-            model.syncdb()
-
         app.config['UPLOAD_PATH'] = tempfile.mkdtemp()
         self.app = app.test_client()
 
     def test_empty_upload(self):
-      self.app.post('/index/attach', data={
-        'file': (StringIO(''), 'empty.txt')
-      })
-      sha = hashlib.sha256()
-      sha.update('')
-      emptySha = sha.hexdigest()
-      uploadedFile = os.path.join(app.config['UPLOAD_PATH'],
-        model.Attachment.hashPath(emptySha, 'empty.txt'))
+        with test_database(test_db, [model.Attachment,
+            model.AttachmentRevision, model.Page]):
+          self.app.post('/index/attach', data={
+            'file': (StringIO(''), 'empty.txt')
+          })
+          sha = hashlib.sha256()
+          sha.update('')
+          emptySha = sha.hexdigest()
+          uploadedFile = os.path.join(app.config['UPLOAD_PATH'],
+            model.Attachment.hashPath(emptySha, 'empty.txt'))
 
-      self.assertTrue(os.path.exists(uploadedFile))
-      resp = self.app.get('/index/file/empty.txt')
-      self.assertEqual(resp.status_code, 200)
-      self.assertEqual(resp.data, '')
+          self.assertTrue(os.path.exists(uploadedFile))
+          resp = self.app.get('/index/file/empty.txt')
+          self.assertEqual(resp.status_code, 200)
+          self.assertEqual(resp.data, '')
 
     def test_simple_upload(self):
-      self.app.post('/index/attach', data={
-        'file': (StringIO('FOOBAR'), 'foo.bar')
-      })
-      sha = hashlib.sha256()
-      sha.update('FOOBAR')
-      emptySha = sha.hexdigest()
-      uploadedFile = os.path.join(app.config['UPLOAD_PATH'],
-        model.Attachment.hashPath(emptySha, 'foo.bar'))
+        with test_database(test_db, [model.Attachment,
+            model.AttachmentRevision, model.Page]):
+          self.app.post('/index/attach', data={
+            'file': (StringIO('FOOBAR'), 'foo.bar')
+          })
+          sha = hashlib.sha256()
+          sha.update('FOOBAR')
+          emptySha = sha.hexdigest()
+          uploadedFile = os.path.join(app.config['UPLOAD_PATH'],
+            model.Attachment.hashPath(emptySha, 'foo.bar'))
 
-      self.assertTrue(os.path.exists(uploadedFile))
-      resp = self.app.get('/index/file/foo.bar')
-      self.assertEqual(resp.status_code, 200)
-      self.assertEqual(resp.data, 'FOOBAR')
+          self.assertTrue(os.path.exists(uploadedFile))
+          resp = self.app.get('/index/file/foo.bar')
+          self.assertEqual(resp.status_code, 200)
+          self.assertEqual(resp.data, 'FOOBAR')
 
     def test_upload_upate(self):
-        self.app.post('/index/attach', data={
-          'file': (StringIO('FOOBAR'), 'foo.bar')
-        })
-        self.app.post('/index/attach', data={
-          'file': (StringIO('BARFOO'), 'foo.bar')
-        })
-        sha = hashlib.sha256()
-        sha.update('BARFOO')
-        emptySha = sha.hexdigest()
-        uploadedFile = os.path.join(app.config['UPLOAD_PATH'],
-          model.Attachment.hashPath(emptySha, 'foo.bar'))
+        with test_database(test_db, [model.Attachment,
+            model.AttachmentRevision, model.Page]):
+            self.app.post('/index/attach', data={
+              'file': (StringIO('FOOBAR'), 'foo.bar')
+            })
+            self.app.post('/index/attach', data={
+              'file': (StringIO('BARFOO'), 'foo.bar')
+            })
+            sha = hashlib.sha256()
+            sha.update('BARFOO')
+            emptySha = sha.hexdigest()
+            uploadedFile = os.path.join(app.config['UPLOAD_PATH'],
+              model.Attachment.hashPath(emptySha, 'foo.bar'))
 
-        self.assertTrue(os.path.exists(uploadedFile))
-        resp = self.app.get('/index/file/foo.bar')
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.data, 'BARFOO')
+            self.assertTrue(os.path.exists(uploadedFile))
+            resp = self.app.get('/index/file/foo.bar')
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.data, 'BARFOO')
