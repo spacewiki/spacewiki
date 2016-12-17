4a1f7a4ff837aee2b9b36b349e004939556b7288
Torrie Fischer <tdfischer@hackerbots.net>
2016-12-17 13:21:34 -0800

    editor: support deleting pages

--- spacewiki/editor.py
+++ spacewiki/editor.py
@@ -1,7 +1,7 @@
 """Routes to handle the realtime editor"""
 
 from flask import (Blueprint, current_app, render_template, request, redirect,
-        url_for)
+        url_for, flash)
 from flask_login import current_user
 from spacewiki import model, auth
 import peewee
@@ -36,9 +36,15 @@ def edit(slug, redirectFrom=None, preview=None, collision=None):
 
         _, title = model.SlugField.split_title(slug)
 
+        deletedPage = None
+        try:
+            deletedPage = model.Page.get(slug=slug, include_deleted=True)
+        except peewee.DoesNotExist:
+            pass
+
         return render_template('404.html',
                                slug=slug,
-                               title=title, page=page,
+                               title=title, page=page, deletedPage=deletedPage,
                                redirectFrom=redirectFrom)
 
 @BLUEPRINT.route("/", methods=['POST'])
@@ -46,6 +52,7 @@ def edit(slug, redirectFrom=None, preview=None, collision=None):
 @auth.tripcodes.tripcode_login_field('author')
 def save(slug=None):
     """Save a new Revision, creating a new Page if needed"""
+    action = request.form.get('action', None)
     newslug = request.form.get('slug', None)
     title = request.form.get('title', None)
     if slug is None:
@@ -58,17 +65,22 @@ def save(slug=None):
         except peewee.DoesNotExist:
             pass
     try:
-        page = model.Page.get(slug=slug)
+        page = model.Page.get(slug=slug, include_deleted=True)
         logging.debug("Updating existing page: %s", page.slug)
         page.title = title
         page.slug = newslug
+        page.deleted = False
         page.save()
     except peewee.DoesNotExist:
         print "Saving '%s' at '%s'" %(title, slug)
         page = model.Page.create(title=title,
                                  slug=slug)
         logging.debug("Created new page: %s (%s)", page.title, page.slug)
-    page.newRevision(request.form['body'], request.form['message'],
-                     current_user._get_current_object())
+    if action == "delete":
+        page.markDeleted(current_user._get_current_object())
+        flash("Page deleted!")
+    else:
+        page.newRevision(request.form['body'], request.form['message'],
+                         current_user._get_current_object())
 
     return redirect(url_for('pages.view', slug=page.slug))
