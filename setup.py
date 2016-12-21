b31a055829dbf57633aca73d507f3f2f35e2b899
Torrie Fischer <tdfischer@hackerbots.net>
2016-12-21 12:33:09 -0800

    requirements: move reqs to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,24 @@
 from setuptools import setup, find_packages
 
 setup(name='spacewiki',
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=[
+          'Flask>=0.11',
+          'MarkupSafe',
+          'peewee>=2.5.1',
+          'bleach',
+          'GitPython',
+          'mistune',
+          'hypothesis',
+          'pillow',
+          'slugify',
+          'flask-script',
+          'colorlog',
+          'gevent',
+          'libsass',
+          'cssmin',
+          'Flask-Assets',
+          'flask-login',
+          'PyYAML'
+      ]
 )
