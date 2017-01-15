d4772fb7931deee0aa18b38350fe6d0e458ddfef
Torrie Fischer <tdfischer@hackerbots.net>
2017-01-15 14:49:28 -0800

    test: softlink: update to latest hypothesis API

--- spacewiki/test/softlink_test.py
+++ spacewiki/test/softlink_test.py
@@ -3,36 +3,48 @@ import werkzeug.test
 import unittest
 from spacewiki import model
 from spacewiki.app import create_app
+from spacewiki.auth import tripcodes
 import logging
-import hypothesis
+from hypothesis import given, assume
+from hypothesis.strategies import text, composite, lists
 import tempfile
 import string
-from hypothesis.extra.fakefactory import FakeFactory
 from playhouse.test_utils import test_database
 from peewee import SqliteDatabase
 
 test_db = SqliteDatabase(':memory:')
 
-def makePath(s):
-    return s.split('://', 1)[1].split('/', 1)[1]
+@composite
+def FullUrl(draw):
+    return 'https://'+draw(Domain())+'/'+draw(Path())
 
-Path = hypothesis.strategy(FakeFactory('url')).map(makePath)
+@composite
+def Path(draw):
+    return '/'.join(draw(lists(PathSegment())))
 
-FullUrl = hypothesis.strategy(FakeFactory('url'))
+@composite
+def PathSegment(draw):
+    return draw(text(alphabet=string.letters, min_size=1))
 
-Domain = hypothesis.strategy(FakeFactory('domain_name'))
+@composite
+def Domain(draw):
+    return '.'.join(draw(lists(DomainSegment(), min_size=2)))
+
+@composite
+def DomainSegment(draw):
+    return draw(text(alphabet=string.letters,min_size=1))+draw(text(alphabet=string.letters+string.digits))
 
 class SoftlinkParsingTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_test_app()
 
-    @hypothesis.given(Path)
+    @given(Path())
     def test_parser_no_refer(self, s):
         req = werkzeug.test.EnvironBuilder(path=s)
         resp = model.Page.parsePreviousSlugFromRequest(req, 'index')
         self.assertEqual(req.lastSlug, None)
 
-    @hypothesis.given(Path, FullUrl)
+    @given(Path(), FullUrl())
     def test_parser_extern_refer(self, page, refer):
         req = werkzeug.test.EnvironBuilder(
             path=page,
@@ -43,13 +55,13 @@ class SoftlinkParsingTestCase(unittest.TestCase):
         resp = model.Page.parsePreviousSlugFromRequest(req, 'index')
         self.assertEqual(req.lastSlug, None)
 
-    @hypothesis.given(Path, Path, Domain, Path)
+    @given(Path(), Path(), Domain(), Path())
     def test_parser_page_refer(self, page, referPage, host, prefix):
         with self.app.app_context():
             req = werkzeug.test.EnvironBuilder(
                 path=prefix+page,
-                headers={'Referer': 'http://'+host+prefix+referPage, 'Host': host},
-                base_url='http://'+host+prefix
+                headers={'Referer': '/'.join(('http://'+host, prefix, referPage)), 'Host': host},
+                base_url='http://'+'/'.join((host, prefix))
             ).get_request()
             resp = model.Page.parsePreviousSlugFromRequest(req, referPage)
             self.assertEqual(req.lastSlug, referPage)
@@ -59,30 +71,33 @@ class SoftlinkTestCase(unittest.TestCase):
         self._app = create_test_app()
         self.app = self._app.test_client()
 
-    @hypothesis.given(Path, Path, Domain, Path)
-    def test_create_softlink(self, src, dest, host, prefix):
-        if src == dest:
-            return
+    @given(Path(), Path(), Domain())
+    def test_create_softlink(self, src, dest, host):
+        assume(src != dest)
+        assume(src != '' and dest != '')
 
-        with test_database(test_db, [model.Softlink, model.Page]):
-            model.Page.create(title='page', slug='page')
-            model.Page.create(title='index', slug='index')
-            self.app.get(
-                prefix+dest,
+        with test_database(test_db, [model.Softlink, model.Page, model.Revision,
+            model.Identity, model.Attachment]):
+            startPage = model.Page.create(title='index', slug=src)
+            endPage = model.Page.create(title='page', slug=dest)
+            with self._app.app_context():
+                startPage.newRevision('', '', tripcodes.new_anon_user())
+                endPage.newRevision('', '', tripcodes.new_anon_user())
+
+            req = self.app.get(
+                '/'+dest,
                 headers={
-                  'Referer': 'http://'+host+prefix+src,
-                  'Host': host
+                  'Referer': 'http://'+host+'/'+src,
                 },
-                base_url = 'http://'+host+prefix
+                base_url = 'http://'+host+'/'
             )
+
+            self.assertEqual(req.status_code, 200)
 
             self.assertTrue(
               model.Softlink.select() \
-                .join(model.Page, on=model.Softlink.src) \
                 .where(
-                    model.Page.slug == src, 
-                ).join(model.Page, on=model.Softlink.dest) \
-                .where(
-                    model.Page.slug == dest,
+                    model.Softlink.src == startPage,
+                    model.Softlink.dest == endPage
                 ).exists()
             )
