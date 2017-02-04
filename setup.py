91f2be72b5928c043f1432e97ee20510708620d4
Torrie Fischer <tdfischer@hackerbots.net>
2017-02-04 12:49:32 -0800

    setup: run npm install as part of build step

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+from distutils.command.build import build as _build
+import distutils.cmd
+import subprocess
+import os.path
+
+class build(_build):
+    sub_commands = [('build_npm', None)] + _build.sub_commands
+
+class NpmCommand(distutils.cmd.Command):
+    user_options = [
+        ('yarn', 'Y', 'Enable to use yarn instead of npm')
+    ]
+
+    def initialize_options(self):
+        self.yarn = False
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        pwd = os.path.dirname(__file__)
+        jsDir = os.path.sep.join((pwd, 'spacewiki', 'static', 'js'))
+        if self.yarn:
+            subprocess.check_call(['yarn', 'install'], cwd=jsDir)
+        else:
+            subprocess.check_call(['npm', 'install'], cwd=jsDir)
 
 extras = {
     'test': ['nose', 'coverage', 'faker', 'hypothesis']
@@ -29,4 +55,8 @@ setup(name='spacewiki',
       ],
       extras_require = extras,
       include_package_data=True,
+      cmdclass={
+          'build_npm': NpmCommand,
+          'build': build
+      }
 )
