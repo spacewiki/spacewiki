41617361bfc516895bd3f7be93d3fcebef1ab192
Torrie Fischer <tdfischer@hackerbots.net>
2017-02-04 03:05:47 -0800

    assets: use absolute path to assets.yml

--- spacewiki/assets.py
+++ spacewiki/assets.py
@@ -1,9 +1,11 @@
 from flask_assets import Environment
 from webassets.filter import register_filter
 from webassets_browserify import Browserify
+import os.path
 
 register_filter(Browserify)
 
 ASSETS = Environment()
 
-ASSETS.from_yaml('assets.yml')
+ASSETS.from_yaml(os.path.sep.join((os.path.dirname(__file__), '..',
+    'assets.yml')))
