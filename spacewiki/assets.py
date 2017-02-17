4d4e149418806ffd6aeffa1a2bb2aad67eb2920e
Torrie Fischer <tdfischer@hackerbots.net>
2017-02-16 17:07:41 -0800

    assets: implement our own browserify filter that works inside static/

--- spacewiki/assets.py
+++ spacewiki/assets.py
@@ -1,9 +1,33 @@
 from flask_assets import Environment
-from webassets.filter import register_filter
-from webassets_browserify import Browserify
+from webassets.filter import register_filter, ExternalTool, option
 import os.path
 
-register_filter(Browserify)
+class LocalBrowserify(ExternalTool):
+    name = 'local-browserify'
+    max_debug_level = None
+    options = {
+        'transforms': option('BROWSERIFY_TRANSFORMS', type=list)
+    }
+
+    def input(self, infile, outfile, **kwargs):
+        args = ['./node_modules/.bin/browserify']
+
+        for transform in self.transforms or []:
+            if isinstance(transform, (list, tuple)):
+                args.extend(('--transform', '['))
+                args.extend(transform)
+                args.append(']')
+            else:
+                args.extend(('--transform', transform))
+
+        args.append(kwargs['source_path'])
+
+        try:
+            self.subprocess(args, outfile, infile, cwd=self.ctx.directory)
+        except Exception, e:
+            raise Exception("Could not find browserify in %s"%(self.ctx.directory), e)
+
+register_filter(LocalBrowserify)
 
 ASSETS = Environment()
 
